9c3da9d4cb69c0d930a876225968040f7c33eaf2
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-05-24 08:49:43 +0200

    100% ok en acabados, falta versión completa

--- generar_crosstab_acabados.py
+++ generar_crosstab_acabados.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     # sheet_coois = 'descarga_coois'
 
     # Configuración de los parámetros del archivo para prod
-    archivo_excel = r"C:\Users\18287\OneDrive - Construcciones y Auxiliar deFerrocarriles SA (CAF)\projects\25-mudanza-ordenes-baan\archivos\descargas\descarga_producción\datos descargados - 23_05_2024.xlsx"
+    archivo_excel = r"C:\Users\18287\OneDrive - Construcciones y Auxiliar deFerrocarriles SA (CAF)\General - CPSL (DIV3)\COMPARATIVA SAP\ACABADOS\ANÁLISIS FALTANTES\analisis_completo_24_05_24\datos descargados_24_05_24.xlsx"
     sheet_bom = 'todo'
     sheet_coois = 'descarga_coois'
 
     # Ejecución del proceso para generar el archivo Excel
+    print({f'Generando crosstabs de materiales en base a {archivo_excel}'})
     archivo_generado = generar_excel_crosstabs_acabados(archivo_excel, sheet_bom, sheet_coois)
     print(f'Archivo generado: {archivo_generado}')
