e195f50a9a06a661625e5331231754d913b92c8f
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-05-28 13:33:32 +0200

    añadido try/catch para verbosidad coherente

--- generar_crosstab_completo.py
+++ generar_crosstab_completo.py
@@ -1,4 +1,4 @@
-# PATH: generar_crosstab_acabados.py
+# PATH: generar_crosstab_completo.py
 
 from src.app.generar_excel_crosstabs_completo import generar_excel_crosstabs_completo
 
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # prod
     # archivo_excel = r"C:\Users\18287\OneDrive - Construcciones y Auxiliar deFerrocarriles SA (CAF)\projects\25-mudanza-ordenes-baan\archivos\descargas\subset\datos descargados - 24_05_2024 - complet.xlsx"
     # dev
-    
+
     archivo_excel = r"C:\Users\18287\OneDrive - Construcciones y Auxiliar deFerrocarriles SA (CAF)\projects\25-mudanza-ordenes-baan\archivos\descargas\subset\datos descargados - 24_05_2024 - complet - subset.xlsx"
     # sheet_fabricacion_eb = 'fabricacion_real_eb'
     # sheet_fabricacion_ea = 'fabricacion_real_ea'
@@ -19,5 +19,3 @@ if __name__ == "__main__":
     }
 
     archivo_generado_ea, archivo_generado_eb = generar_excel_crosstabs_completo(**args)
-    print(f'Archivo generado para EA: {archivo_generado_ea}')
-    print(f'Archivo generado para EB: {archivo_generado_eb}')
--- src/app/generar_excel_crosstabs_completo.py
+++ src/app/generar_excel_crosstabs_completo.py
@@ -6,36 +6,45 @@ from src.service.generar_crosstab_modelo_materiales import cargar_datos, prepara
 from src.service.formato_crosstab import format_crosstabs, agregar_cantidad_bom_header, formato_indice, agregar_enlace_indice, agregar_enlace_indice_hoja
 
 def generar_excel_crosstabs_completo(archivo, sheet_bom_ea, sheet_bom_eb, sheet_coois):
-    print('Cargando datos...')
-    bom_ea, bom_eb, coois = cargar_datos(archivo, sheet_bom_ea, sheet_bom_eb, sheet_coois)
-    coois_ea, coois_eb = preparar_datos_coois(coois)
+    try:
+        print('Cargando datos...')
+        bom_ea, bom_eb, coois = cargar_datos(archivo, sheet_bom_ea, sheet_bom_eb, sheet_coois)
+        coois_ea, coois_eb = preparar_datos_coois(coois)
 
-    results = []
-    for bom, coois, subset_name in [(bom_ea, coois_ea, 'EA'), (bom_eb, coois_eb, 'EB')]:
-        print(f'Generando archivo Excel para {subset_name}...')
-        wb = Workbook()
-        wb.remove(wb.active)  # Elimina la pestaña predeterminada
+        results = []
+        for bom, coois, subset_name in [(bom_ea, coois_ea, 'EA'), (bom_eb, coois_eb, 'EB')]:
+            try:
+                print(f'Generando archivo Excel para {subset_name}...')
+                wb = Workbook()
+                wb.remove(wb.active)  # Elimina la pestaña predeterminada
 
-        index_sheet = wb.create_sheet(title="Índice", index=0)
-        index_sheet.append(["Modelo"])
-        unique_modelos = sorted(bom['Modelo'].unique())
+                index_sheet = wb.create_sheet(title="Índice", index=0)
+                index_sheet.append(["Modelo"])
+                unique_modelos = sorted(bom['Modelo'].unique())
 
-        for i, modelo in enumerate(unique_modelos, start=2):
-            ws = wb.create_sheet(title=modelo)
-            crosstab = generar_crosstab_modelo_materiales(bom, coois, modelo)
-            for row in dataframe_to_rows(crosstab, index=True, header=True):
-                ws.append(row)
-            agregar_cantidad_bom_header(ws, bom, modelo)
-            format_crosstabs(ws, bom, modelo)
+                for i, modelo in enumerate(unique_modelos, start=2):
+                    ws = wb.create_sheet(title=modelo)
+                    crosstab = generar_crosstab_modelo_materiales(bom, coois, modelo)
+                    for row in dataframe_to_rows(crosstab, index=True, header=True):
+                        ws.append(row)
+                    agregar_cantidad_bom_header(ws, bom, modelo)
+                    format_crosstabs(ws, bom, modelo)
 
-            # Agregar enlace al índice y darle formato
-            agregar_enlace_indice(index_sheet, modelo, i)
-            agregar_enlace_indice_hoja(ws)
+                    # Agregar enlace al índice y darle formato
+                    agregar_enlace_indice(index_sheet, modelo, i)
+                    agregar_enlace_indice_hoja(ws)
 
-        formato_indice(index_sheet)
-        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = f'./results/crosstabs_{subset_name}/crosstabs_materiales_{subset_name}_{timestamp}.xlsx'
-        wb.save(filename)
-        results.append(filename)
+                formato_indice(index_sheet)
+                timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+                filename = f'./results/crosstabs_{subset_name}/crosstabs_materiales_{subset_name}_{timestamp}.xlsx'
+                wb.save(filename)
+                results.append(filename)
+                print(f'Archivo generado para {subset_name}: {filename}')
+            except Exception as e:
+                print(f'Ocurrió un error generando el archivo para {subset_name}: {e}')
+                results.append(None)
 
-    return results
+        return results
+    except Exception as e:
+        print(f'Ocurrió un error en la función principal: {e}')
+        return None, None
