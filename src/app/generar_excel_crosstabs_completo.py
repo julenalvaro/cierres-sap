4a591927dcfa7d36cd4eaf843f0e30bfe011bebf
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-05-30 08:56:23 +0200

    link tree

--- src/app/generar_excel_crosstabs_completo.py
+++ src/app/generar_excel_crosstabs_completo.py
@@ -2,11 +2,32 @@ import os
 import traceback
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Font
+from openpyxl.worksheet.hyperlink import Hyperlink
 
 from src.config.config import obtener_configuracion
 from src.service.generar_crosstab_modelo_materiales import cargar_datos, transformar_coois, transformar_stocks, transformar_fabricacion_real, generar_crosstab_modelo_materiales
 from src.service.formato_crosstab import format_crosstabs, agregar_cantidad_bom_header, formato_indice, agregar_enlace_indice, agregar_enlace_indice_hoja, guardar_excel
 from src.service.transformar_bom_a_arbol_correcciones import transformar_bom_a_arbol_correcciones
+
+def agregar_enlace_arbol(ws, modelos):
+    # Encontrar la columna que contiene el encabezado "Modelo"
+    modelo_col = None
+    for cell in ws[1]:
+        if cell.value == "Modelo":
+            modelo_col = cell.column_letter
+            break
+
+    if modelo_col is None:
+        raise ValueError("No se encontró una columna con el encabezado 'Modelo'")
+
+    # Agregar enlaces a las celdas en la columna "Modelo"
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
+        cell = row[ws[modelo_col + '1'].col_idx - 1]  # Obtener la celda en la columna "Modelo" de la fila actual
+        if cell.value in modelos:
+            modelo = cell.value
+            cell.hyperlink = Hyperlink(ref=f'{modelo_col}{cell.row}', target=f'#\'{modelo}\'!A1', tooltip=f'Ir a {modelo}')
+            cell.font = Font(color='0000FF', underline='single')
 
 def generar_excel_crosstabs_completo(archivo, sheet_bom_ea, sheet_bom_eb, sheet_coois, sheet_stocks, sheet_fabricacion_real_ea, sheet_fabricacion_real_eb):
     config = obtener_configuracion()
@@ -73,11 +94,14 @@ def generar_excel_crosstabs_completo(archivo, sheet_bom_ea, sheet_bom_eb, sheet_
                 for row in dataframe_to_rows(arbol_correcciones, index=False, header=True):
                     arbol_ws.append(row)
 
+                # Agregar enlaces en la hoja del árbol de correcciones
+                agregar_enlace_arbol(arbol_ws, unique_modelos)
+
                 formato_indice(index_sheet)
 
                 # Reordenar hojas: árbol de correcciones primero, luego índice, luego crosstabs
                 wb._sheets.sort(key=lambda sheet: sheet.title not in [f'arbol_correcciones_{subset_name}', "Índice"])
-                
+
                 filename = guardar_excel(wb, dir_crosstabs, subset_name)
                 results.append(filename)
                 print(f'Archivo generado para {subset_name}: {filename}')
