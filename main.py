46b95946847648992b66ca98b2fa465d3d8ad331
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-06-05 22:04:34 +0200

    chore: Configure CORS middleware for Excel Generation API

--- main.py
+++ main.py
@@ -2,8 +2,18 @@
 
 from fastapi import FastAPI
 from src.app.routers.excel_router import router as excel_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Excel Generation API', version='1.0', description='API for generating and downloading Excel files.')
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Lista de dominios si quieres restringir, usa '*' para desarrollo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluir el router del módulo Excel
 app.include_router(excel_router, prefix="/excel", tags=["Excel Operations"])
