ae6229726b77c7561e66665e1386787cbd803a23
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-06-06 09:10:02 +0200

    loader frontend

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,17 +3,19 @@
 // src/App.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Checkbox, FormControlLabel, Typography, Box, Container, TextField } from '@mui/material';
+import { Button, Checkbox, FormControlLabel, Typography, Box, Container, TextField, CircularProgress, Backdrop } from '@mui/material';
 
 function App() {
   const [archivoStocks, setArchivoStocks] = useState(null);
   const [archivoCoois, setArchivoCoois] = useState(null);
   const [downloadEA, setDownloadEA] = useState(true);
   const [downloadEB, setDownloadEB] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const handleFileChange = (e, setter) => setter(e.target.files[0]);
 
   const handleSubmit = async () => {
+    setLoading(true);
     const formData = new FormData();
     formData.append('archivo_stocks', archivoStocks);
     formData.append('archivo_coois', archivoCoois);
@@ -32,6 +34,8 @@ function App() {
       link.click();
     } catch (error) {
       console.error('Error al generar el archivo:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -39,19 +43,8 @@ function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" gutterBottom>
-          Generador de Archivos Excel
+          Correcciones SAP División 3
         </Typography>
-        <TextField
-          type="file"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          onChange={(e) => handleFileChange(e, setArchivoStocks)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          label="Cargar Archivo Stocks"
-        />
         <TextField
           type="file"
           fullWidth
@@ -61,7 +54,18 @@ function App() {
           InputLabelProps={{
             shrink: true,
           }}
-          label="Cargar Archivo Coois"
+          label="Cargar Archivo de órdenes de producción (COOIS)"
+        />
+        <TextField
+          type="file"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          onChange={(e) => handleFileChange(e, setArchivoStocks)}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="Cargar Archivo Stocks (Opcional)"
         />
         <FormControlLabel
           control={<Checkbox checked={downloadEA} onChange={() => setDownloadEA(!downloadEA)} />}
@@ -74,9 +78,15 @@ function App() {
         <Button variant="contained" color="primary" fullWidth onClick={handleSubmit} sx={{ mt: 2 }}>
           Generar
         </Button>
+        {loading && (
+          <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
+            <CircularProgress color="inherit" />
+          </Backdrop>
+        )}
       </Box>
     </Container>
   );
 }
 
 export default App;
+
